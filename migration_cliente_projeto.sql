a34debf5efff2aedc76331f83c8fdb1103315e6d
TADEU-BASS <tadeu.baixo@gmail.com>
2026-05-05 20:02:33 -0300

    Delete migration_cliente_projeto.sql

--- migration_cliente_projeto.sql
+++
@@ -1,6 +0,0 @@
-USE capabilidade_ws;
-
-ALTER TABLE ensaios
-  ADD COLUMN cliente_projeto VARCHAR(150) NULL AFTER data_ensaio;
-
-CREATE INDEX idx_ensaios_cliente_projeto ON ensaios (cliente_projeto);
